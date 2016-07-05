abac170db8e9aeef65cc546d8abc91da0e73eff3
karajrish <rishabh.karajgi@gmail.com>
2016-07-05 21:36:08 +0530

    pushed files

---
+++ Arrays/FirstMissingInteger/main.cpp
@@ -0,0 +1,23 @@
+void swap(int &a, int &b){
+    int temp;
+    temp = a; 
+    a = b;
+    b = temp;
+}
+int Solution::firstMissingPositive(vector<int> &A) {
+     int n = A.size();
+            for (int i = 0; i < n; i++) {
+                if (A[i] > 0 && A[i] <= n) {
+                    int pos = A[i] - 1;
+                    if (A[pos] != A[i]) {
+                        swap(A[pos], A[i]);
+                        i--;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                if (A[i] != i + 1) return (i + 1);
+            }
+            return n + 1;
+}
+
