ad17b84204a0259288376a800f64bdb4b2962d3f
karajrish <rishabh.karajgi@gmail.com>
2016-07-18 15:52:01 +0530

    pushed files

---
+++ Linked Lists/Partition List/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::partition(ListNode* head, int x) {
+    
+    if (!head) return NULL;
+            ListNode * iterator = head; 
+
+            ListNode * start = new ListNode(0); // list of nodes greater than x
+            ListNode * tail = start;
+
+            ListNode * newHead = new ListNode(0); 
+            newHead -> next = head;
+            ListNode * pre = newHead; // previous node, we need it for removing
+
+
+            while (iterator) {
+                if (iterator -> val >= x) {
+                    pre -> next = iterator -> next; // remove from our list
+                    tail -> next = iterator; // add to list of nodes greater than x
+                    tail = tail -> next;
+                    iterator = iterator -> next;
+                    tail -> next = NULL;
+                }
+                else
+                    pre = iterator, iterator = iterator -> next;
+            }
+            pre -> next = start -> next;
+            return newHead -> next;
+}
+
