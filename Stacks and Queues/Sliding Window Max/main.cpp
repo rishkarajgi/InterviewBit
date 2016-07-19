d152518b665ca34b9a6f7518411e0f438f240111
karajrish <rishabh.karajgi@gmail.com>
2016-07-20 02:19:45 +0530

    Postfix evaluation

---
+++ Stacks and Queues/Sliding Window Max/main.cpp
@@ -0,0 +1,25 @@
+vector<int> Solution::slidingMaximum(const vector<int> &A, int w) {
+    
+    int n = A.size();
+            vector<int> B;
+            if (n < w) return B;
+            B.resize(n - w + 1);
+            deque<int> Q;
+            for (int i = 0; i < w; i++) {
+                while (!Q.empty() && A[i] >= A[Q.back()])
+                    Q.pop_back();
+                Q.push_back(i);
+            }
+            for (int i = w; i < n; i++) {
+                B[i - w] = A[Q.front()];
+                while (!Q.empty() && A[i] >= A[Q.back()])
+                    Q.pop_back();
+                while (!Q.empty() && Q.front() <= i - w)
+                    Q.pop_front();
+                Q.push_back(i);
+            }
+            B[n - w] = A[Q.front()];
+            return B;
+    
+}
+
